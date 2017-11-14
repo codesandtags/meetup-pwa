77cca0ac90155cd03ec59f18ddbe86ed7ed19918
Edwin Torres <codesandtags@gmail.com>
2017-11-13 22:15:00 -0500

    feat: Changed colors

--- src/app/components/meetup/meetup-card/meetup-card.component.ts
+++ src/app/components/meetup/meetup-card/meetup-card.component.ts
@@ -8,11 +8,28 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MeetupCardComponent implements OnInit {
 
   @Input() meetup;
+  category;
 
   constructor() {
   }
 
   ngOnInit() {
+    switch (this.meetup.event.category) {
+      case 'dance':
+        this.category = 'fa-music';
+        break;
+      case 'technology':
+        this.category = 'fa-laptop';
+        break;
+      case 'tourism':
+        this.category = 'fa-globe';
+        break;
+      case 'food':
+        this.category = 'fa-cutlery';
+        break;
+      default :
+        this.category = 'fa-calendar';
+    }
   }
 
 }
