2e43ea0d11781474a3b685ba4efaf1baaf409b02
Edwin Torres <codesandtags@gmail.com>
2017-11-25 19:02:51 -0500

    feat: Show a notificationc as example

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -22,6 +22,12 @@ export class HeaderComponent implements OnInit {
     Notification.requestPermission(function (result) {
       if (result !== 'granted') {
         console.info('The user has not granted the notifications');
+      } else {
+        const options = {
+          body: 'You have subscribed to the notifications'
+        };
+
+        new Notification('Successfully subscribed!', options);
       }
     });
   }
